ebe3db9887df003102b6cb75adc68df293e3f034
sue445 <sue445@sue445.net>
2026-04-25 19:42:54 +0900

    Fixes response.body pointer

--- src/statistics.zig
+++ src/statistics.zig
@@ -288,7 +288,10 @@ fn getReposByYear(
             ),
         },
     );
-    defer context.client.allocator.free(response.body);
+
+    const graphql_body = response.body;
+    defer context.client.allocator.free(graphql_body);
+
     if (response.status != .ok) {
         std.log.err(
             "Failed to get data from {d} ({?s})",
@@ -324,7 +327,7 @@ fn getReposByYear(
             },
         } } },
         context.arena.allocator(),
-        response.body,
+        graphql_body,
         .{ .ignore_unknown_fields = true, .allocate = .alloc_always },
     )).data.viewer.contributionsCollection;
     std.log.info(
